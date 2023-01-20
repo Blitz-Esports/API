d57a83a574b48ae5197432ebbb9797bbcfc76277
JORDAAR <69628820+Jordaar@users.noreply.github.com>
2023-01-20 11:40:30 +0530

    Delete routes directory

--- routes/resize.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require("express");
-const { expressSharp, HttpAdapter } = require("express-sharp");
-
-module.exports.ResizeRoute = Router().use("/resize", expressSharp({
-    imageAdapter: new HttpAdapter()
-}));
