5031cc374461cc3fd8ef006bb8a910055976d6c8
JORDAAR <69628820+Jordaar@users.noreply.github.com>
2023-01-20 11:40:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 require("dotenv").config();
+const { version, cache } = require("./config");
 const express = require("express");
+const util = require('util');
+const glob = util.promisify(require('glob'));
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const { BlogRoute } = require("./routes/blog");
-const { ResizeRoute } = require("./routes/resize");
-const { FaqRoute } = require("./routes/faq");
-const { GalleryRoute } = require("./routes/gallery");
-const { HeaderRoute } = require("./routes/header");
-const app = express();
 const { middleware } = require("apicache");
-const { ContentRoute } = require("./routes/content");
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-app.use(middleware("1 hour"));
+if (cache.enabled) app.use(middleware(cache.duration));
 
 const run = async () => {
 
@@ -23,12 +19,30 @@ const run = async () => {
         res.send("Hello World");
     });
 
-    app.use(BlogRoute);
-    app.use(ResizeRoute);
-    app.use(FaqRoute);
-    app.use(GalleryRoute);
-    app.use(HeaderRoute);
-    app.use(ContentRoute);
+    const filePaths = await glob(`routes/${version}/**/*.js`, { nodir: true });
+    const routes = filePaths.map((file) => {
+        return {
+            path: "./" + file,
+            route: file
+                .replace("routes", "")
+                .replace(".js", "")
+        }
+    });
+
+    routes.forEach((r) => {
+        const file = require(r.path);
+        const defRoute = file.route ?? r.route;
+        if (file.type === "GET") app.get(defRoute, (...arg) => {
+            try {
+                file.fn(...arg);
+            } catch (e) {
+                console.log(e);
+                arg[1].status(500).send("An unexpected error has ocurred");
+            }
+        })
+        if (file.type === "APP") app.use(defRoute, file.fn)
+        console.log(`Loaded: ${r.route}`);
+    });
 
     app.listen(process.env.PORT || 3000, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
